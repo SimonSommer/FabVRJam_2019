0a675439ba919e03582f2357ba5a7c5addbaea70
LB31 <leonidb93@gmail.com>
2019-02-23 16:39:13 +0100

    add pathfinding

---
+++ Assets/Scripts/TurtleSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleSpawner : MonoBehaviour
+{
+    public GameObject TurtleModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(TurtleModel, transform.position, transform.rotation);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
