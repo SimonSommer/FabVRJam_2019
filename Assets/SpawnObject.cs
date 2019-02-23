5640ae074eff807422ed892b43bfd0380e92a3b6
Simon <fate1990@googlemail.com>
2019-02-23 18:26:04 +0100

    spawn dummy tower

---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(objectToSpawn, transform.position, transform.rotation);
+    }
+}
